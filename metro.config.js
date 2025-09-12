8239b1f6a2401d6326d6e8106484b84069840343
hassanmubiru <hassanmubiru7@gmail.com>
2025-09-12 22:32:20 +0300

    modified:   metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -11,14 +11,4 @@ config.resolver.assetExts.push(
 // Add support for additional source extensions
 config.resolver.sourceExts.push('ts', 'tsx', 'js', 'jsx', 'json');
 
-// Fix for web platform - exclude expo-router from web builds
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
-// Web-specific configuration
-config.resolver.alias = {
-  ...config.resolver.alias,
-  // Exclude expo-router for web
-  'expo-router': false,
-};
-
 module.exports = config;
